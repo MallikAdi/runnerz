edea791a9e989d897e3fb6fc62575a53651c0547
MallikAdi <aditya.mallik.10@gmail.com>
2024-08-29 20:41:40 +0530

    Setup findById and findAll

---
+++ src/main/java/dev/adimallik/runnerz/run/RunRepository.java
@@ -0,0 +1,45 @@
+package dev.adimallik.runnerz.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+    // in-memory storage of all runs
+    private List<Run> runs = new ArrayList<>();
+
+    // this function returns all runs available in memory
+    List<Run> findAll() {
+        return runs;
+    }
+
+    Run findById(Integer id){
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst()
+                .get();
+    }
+
+    // this initializes some runs for us once the app is running
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1,
+                "Monday Morning Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
+                3,
+                Location.INDOOR));
+
+        runs.add(new Run(2,
+                "Wednesday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
+                6,
+                Location.OUTDOOR));
+    }
+}
